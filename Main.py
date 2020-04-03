aaae3fd84aa412e102af47a36e119ba35bf81523
Michael Tang <michael.tang02@sap.com>
2020-04-03 11:19:44 -0400

    Refactoring with Dictionary

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import Vocabulary
 import gram
 from collections import Counter
 import math
-
+import evaluation
 
 def countWords(dictionary):
     count = 0
@@ -16,21 +16,94 @@ def addSmoothValue(languagelist, smooth_value):
         languagelist[key] = value + smooth_value
 
 
-enList = []
-esList = []
-euList = []
-ptList = []
-caList = []
-glList = []
+languages = {
+    "en": {
+        "count": 0,
+        "list": [],
+        "words": 0,
+        "tweetProb": 0,
+        "score": 0,
+        "precision": 0,
+        "precisionDen": 0,
+        "precisionNum": 0,
+        "recall": 0,
+        "recallDen": 0,
+        "recallNum": 0,
+        "f1": 0
+    },
+    "es": {
+        "count": 0,
+        "list": [],
+        "words": 0,
+        "tweetProb": 0,
+        "score": 0,
+        "precision": 0,
+        "precisionDen": 0,
+        "precisionNum": 0,
+        "recall": 0,
+        "recallDen": 0,
+        "recallNum":0,
+        "f1": 0
+    },
+    "pt": {
+        "count": 0,
+        "list": [],
+        "words": 0,
+        "tweetProb": 0,
+        "score": 0,
+        "precision": 0,
+        "precisionDen": 0,
+        "precisionNum": 0,
+        "recall": 0,
+        "recallDen": 0,
+        "recallNum": 0,
+        "f1": 0
+    },
+    "eu": {
+        "count": 0,
+        "list": [],
+        "words": 0,
+        "tweetProb": 0,
+        "score": 0,
+        "precision": 0,
+        "precisionDen": 0,
+        "precisionNum": 0,
+        "recall": 0,
+        "recallDen": 0,
+        "recallNum": 0,
+        "f1": 0
+    },
+    "ca": {
+        "count": 0,
+        "list": [],
+        "words": 0,
+        "tweetProb": 0,
+        "score": 0,
+        "precision": 0,
+        "precisionDen": 0,
+        "precisionNum": 0,
+        "recall": 0,
+        "recallDen": 0,
+        "recallNum": 0,
+        "f1": 0
+    },
+    "gl": {
+        "count": 0,
+        "list": [],
+        "words": 0,
+        "tweetProb": 0,
+        "score": 0,
+        "precision": 0,
+        "precisionDen": 0,
+        "precisionNum": 0,
+        "recall": 0,
+        "recallDen": 0,
+        "recallNum": 0,
+        "f1": 0
+    }
+}
 
-enCount = 0
-esCount = 0
-euCount = 0
-ptCount = 0
-caCount = 0
-glCount = 0
-
-vocabulary = 1
+vocabulary = 0
 whichGram = 1
 smooth_value = 0.1
 # accuracy
@@ -38,167 +111,41 @@ accuracy_numerator = 0
 number_tweet = 0
 accuracy_percentage = 0
 
-# precision
-es_precision = 0
-es_precision_num = 0
-es_precision_den = 0
-
-gl_precision = 0
-gl_precision_num = 0
-gl_precision_den = 0
-
-eu_precision = 0
-eu_precision_num = 0
-eu_precision_den = 0
-
-pt_precision = 0
-pt_precision_num = 0
-pt_precision_den = 0
-
-en_precision = 0
-en_precision_num = 0
-en_precision_den = 0
-
-ca_precision = 0
-ca_precision_num = 0
-ca_precision_den = 0
-
-# Recall
-es_recall = 0
-es_recall_num = 0
-es_recall_den = 0
-
-gl_recall = 0
-gl_recall_num = 0
-gl_recall_den = 0
-
-eu_recall = 0
-eu_recall_num = 0
-eu_recall_den = 0
-
-pt_recall = 0
-pt_recall_num = 0
-pt_recall_den = 0
-
-en_recall = 0
-en_recall_num = 0
-en_recall_den = 0
-
-ca_recall = 0
-ca_recall_num = 0
-ca_recall_den = 0
-
-# F1
-eu_f1 = 0
-en_f1 = 0
-es_f1 = 0
-pt_f1 = 0
-gl_f1 = 0
-ca_f1 = 0
-
 solution_file_name = "trace_" + str(vocabulary) + "_" + str(whichGram) + "_" + str(smooth_value) + ".txt"
-solution_file = open(solution_file_name, 'w+', encoding='utf-8')
+solution_file = open("trace/" + solution_file_name, 'w+', encoding='utf-8')
+
+training_tweet = "training-tweets.txt"
+test_tweet = "test-tweets.given.txt"
 
 # Create the dictionary of words and their count
-for i in Vocabulary.read("training-tweets.txt"):
+for i in Vocabulary.read(training_tweet):
     # create a dictionary of uni/bi/tri-gram
-    arr = gram.gram(i.message, whichGram)
-    if i.language == 'en':
-        enCount += 1
-        enList += arr
-    elif i.language == 'es':
-        esCount += 1
-        esList += arr
-    elif i.language == 'ca':
-        caCount += 1
-        caList += arr
-    elif i.language == 'pt':
-        ptCount += 1
-        ptList += arr
-    elif i.language == 'eu':
-        euCount += 1
-        euList += arr
-    elif i.language == 'gl':
-        glCount += 1
-        glList += arr
-
-# use Counter library to create dictionary with unique keys (character) and the count of the keys
-esList = (Counter(esList))
-enList = (Counter(enList))
-ptList = (Counter(ptList))
-euList = (Counter(euList))
-caList = (Counter(caList))
-glList = (Counter(glList))
-
-# add the smooth value to each value of the key in the list
-addSmoothValue(esList, smooth_value)
-addSmoothValue(enList, smooth_value)
-addSmoothValue(ptList, smooth_value)
-addSmoothValue(euList, smooth_value)
-addSmoothValue(caList, smooth_value)
-addSmoothValue(glList, smooth_value)
-
-# Count how many words there is in that language
-enWords = countWords(enList)
-esWords = countWords(esList)
-ptWords = countWords(ptList)
-euWords = countWords(euList)
-caWords = countWords(caList)
-glWords = countWords(glList)
+    languages[i.language]["count"] += 1
+    languages[i.language]["list"] += gram.gram(i.message, whichGram)
 
 # create the probability of each language for a tweet
-totalTweet = enCount + esCount + ptCount + euCount + caCount + glCount
-enTweetProb = enCount / totalTweet
-esTweetProb = esCount / totalTweet
-ptTweetProb = ptCount / totalTweet
-euTweetProb = euCount / totalTweet
-caTweetProb = caCount / totalTweet
-glTweetProb = glCount / totalTweet
+totalTweet = 0
+for key, value in languages.items():
+    totalTweet += languages[key]["count"]
+# use Counter library to create dictionary with unique keys (character) and the count of the keys and add smooth value
+for key, value in languages.items():
+    languages[key]["list"] = (Counter(languages[key]["list"]))
+    addSmoothValue(languages[key]["list"], smooth_value)
+    languages[key]["words"] = countWords(languages[key]["list"])
+    languages[key]["tweetProb"] = languages[key]["count"] / totalTweet
 
 # Evaluate Test tweets
-for i in Vocabulary.read("test-tweets-given.txt"):
-    print('=====================')
+for i in Vocabulary.read(training_tweet):
     tweetId = i.tweetId
     language = i.language
     test = gram.gram(i.message, whichGram)
-    # i = 439379404574453760	aritzabrodes	es	@AnderDelPozo @PesqueWhite hahaha yo tambien me he quedao pillao ahahha
-    # when gram = 1, apply bayes formula with log of 10
     if whichGram == 1:
-        enScore = enTweetProb
+        for key, value in languages.items():
+            languages[key]["score"] = math.log(languages[key]["tweetProb"], 10)
         for i in test:
-            enScore += math.log(((enList.get(i, smooth_value)) / enWords), 10)
-        print("===EN===")
-        print(enScore)
-
-        esScore = esTweetProb
-        for i in test:
-            esScore += math.log(((esList.get(i, smooth_value)) / esWords), 10)
-        print("===ES===")
-        print(esScore)
-
-        ptScore = ptTweetProb
-        for i in test:
-            ptScore += math.log(((ptList.get(i, smooth_value)) / ptWords), 10)
-        print("===PT===")
-        print(ptScore)
-
-        euScore = euTweetProb
-        for i in test:
-            euScore += math.log(((euList.get(i, smooth_value)) / euWords), 10)
-        print("===EU===")
-        print(euScore)
-
-        caScore = caTweetProb
-        for i in test:
-            caScore += math.log(((caList.get(i, smooth_value)) / caWords), 10)
-        print("===CA===")
-        print(caScore)
-
-        glScore = glTweetProb
-        for i in test:
-            glScore += math.log(((glList.get(i, smooth_value)) / glWords), 10)
-        print("===GL===")
-        print(glScore)
+            for key, value in languages.items():
+                languages[key]["score"] += math.log(
+                    ((languages[key]["list"].get(i, smooth_value)) / languages[key]["words"]), 10)
 
     # calculate probability when gram is 2 for each language
     # for loop is to find the probability of the first character of the bigram
@@ -568,158 +515,41 @@ for i in Vocabulary.read("test-tweets-given.txt"):
     # It needs: tweet Id, the guess, the value, the real language, wrong/correct
     guess = ""
     label = ""
-    largest_number = max(enScore, esScore, ptScore, glScore, caScore, euScore)
-    if largest_number == enScore:
-        guess = "en"
-    elif largest_number == esScore:
-        guess = "es"
-    elif largest_number == glScore:
-        guess = "gl"
-    elif largest_number == ptScore:
-        guess = "pt"
-    elif largest_number == euScore:
-        guess = "eu"
-    elif largest_number == caScore:
-        guess = "ca"
+    largest_number = max(languages["es"]["score"], languages["en"]["score"], languages["pt"]["score"],
+                         languages["ca"]["score"], languages["gl"]["score"], languages["eu"]["score"])
+    for key, value in languages.items():
+        if languages[key]["score"] == largest_number:
+            guess = key
     if guess == language:
         label = "correct"
     else:
         label = "wrong"
+
     # TODO read those values dynamically if needed (Double check)
     solution_line = tweetId + "  " + str(guess) + " " + "{:.2e}".format(
         largest_number) + "  " + str(language) + "  " + str(label) + "\n"
     solution_file.write(solution_line)
     number_tweet += 1
+
     # TODO Write to Evaluation file
     # accuracy
     if label == "correct":
         accuracy_numerator += 1
 
-    # Precision
-    if guess == "es":
-        es_precision_den += 1
-        if label == "correct":
-            es_precision_num += 1
-
-    if guess == "ca":
-        ca_precision_den += 1
-        if label == "correct":
-            ca_precision_num += 1
-
-    if guess == "en":
-        en_precision_den += 1
-        if label == "correct":
-            en_precision_num += 1
-
-    if guess == "eu":
-        eu_precision_den += 1
-        if label == "correct":
-            eu_precision_num += 1
-
-    if guess == "gl":
-        gl_precision_den += 1
-        if label == "correct":
-            gl_precision_num += 1
-
-    if guess == "pt":
-        pt_precision_den += 1
-        if label == "correct":
-            pt_precision_num += 1
-
-    # Recall 
-    if language == "es":
-        es_recall_den += 1
-        if label == "correct":
-            es_recall_num += 1
-
-    if language == "en":
-        en_recall_den += 1
-        if label == "correct":
-            en_recall_num += 1
-
-    if language == "pt":
-        pt_recall_den += 1
-        if label == "correct":
-            pt_recall_num += 1
-
-    if language == "eu":
-        eu_recall_den += 1
-        if label == "correct":
-            eu_recall_num += 1
-
-    if language == "ca":
-        ca_recall_den += 1
-        if label == "correct":
-            ca_recall_num += 1
-
-    if language == "gl":
-        gl_recall_den += 1
-        if label == "correct":
-            gl_recall_num += 1
+    for key, value in languages.items():
+        # Precision
+        if guess == key:
+            languages[key]["precisionDen"] += 1
+            if label == "correct":
+                languages[key]["precisionNum"] += 1
+        # Recall
+        if language == key:
+            languages[key]["recallDen"] += 1
+            if label == "correct":
+                languages[key]["recallNum"] += 1
 
 accuracy_percentage = accuracy_numerator / number_tweet
 
-en_precision = en_precision_num / en_precision_den
-eu_precision = eu_precision_num / eu_precision_den
-gl_precision = gl_precision_num / gl_precision_den
-pt_precision = pt_precision_num / pt_precision_den
-es_precision = es_precision_num / es_precision_den
-ca_precision = ca_precision_num / ca_precision_den
+evaluation.writeToEvaluationFile(languages, whichGram, number_tweet, smooth_value, vocabulary, accuracy_percentage)
 
-gl_recall = gl_recall_num / gl_recall_den
-ca_recall = ca_recall_num / ca_recall_den
-eu_recall = eu_recall_num / eu_recall_den
-pt_recall = pt_recall_num / pt_recall_den
-es_recall = es_recall_num / es_recall_den
-en_recall = en_recall_num / en_recall_den
-# F1-measure
-try:
-    es_f1 = (2 * es_precision * es_recall) / (es_precision + es_recall)
-except:
-    es_f1 = 0
-try:
-    gl_f1 = (2 * gl_precision * gl_recall) / (gl_precision + gl_recall)
-except:
-    gl_f1 = 0
-try:
-    pt_f1 = (2 * pt_precision * pt_recall) / (pt_precision + pt_recall)
-except:
-    pt_f1 = 0
-try:
-    ca_f1 = (2 * ca_precision * ca_recall) / (ca_precision + ca_recall)
-except:
-    ca_f1 = 0
-try:
-    eu_f1 = (2 * eu_precision * eu_recall) / (eu_precision + eu_recall)
-except:
-    eu_f1 = 0
-try:
-    en_f1 = (2 * en_precision * en_recall) / (en_precision + en_recall)
-except:
-    en_f1 = 0
-
-# Macro F1 weighted-average F1
-macro_f1 = (es_f1 + en_f1 + eu_f1 + pt_f1 + ca_f1 + gl_f1) / 6
-weight_f1 = (
-                    es_recall_den * es_f1 + en_precision_den * en_f1 + eu_recall_den * eu_f1 + ca_recall_den * ca_f1 + gl_recall_den * gl_f1) / number_tweet
-
-eval_file_name = "eval_" + str(vocabulary) + "_" + str(whichGram) + "_" + str(smooth_value) + ".txt"
-
-eval_file = open(eval_file_name, 'w+',  encoding='utf-8')
-accuracy_line = str(accuracy_percentage) + "\n"
-eval_file.write(accuracy_line)
-
-precision_line = str(eu_precision) + "  " + str(ca_precision) + "  " + str(gl_precision) + "  " + str(
-    es_precision) + "  " + str(en_precision) + "  " + str(pt_precision) + "  " + "\n"
-eval_file.write(precision_line)
-
-recall_line = str(eu_recall) + "  " + str(ca_recall) + "  " + str(gl_recall) + "  " + str(
-    es_recall) + "  " + str(en_recall) + "  " + str(pt_recall) + "  " + "\n"
-eval_file.write(recall_line)
-
-f1_line = str(eu_f1) + "  " + str(ca_f1) + "  " + str(gl_f1) + "  " + str(
-    es_f1) + "  " + str(en_f1) + "  " + str(pt_f1) + "  " + "\n"
-eval_file.write(f1_line)
-
-macro_average_line = str(macro_f1) + "  " + str(weight_f1)
-eval_file.write(macro_average_line)
+print("Done")
---
+++ evaluation.py
@@ -0,0 +1,39 @@
+def writeToEvaluationFile(languages, whichGram, number_tweet, smooth_value, vocabulary, accuracy_percentage):
+    macro_f1 = 0
+    weight_f1 = 0
+    for key, value in languages.items():
+        languages[key]["precision"] = languages[key]["precisionNum"] / languages[key]["precisionDen"]
+        languages[key]["recall"] = languages[key]["recallNum"] / languages[key]["recallDen"]
+        languages[key]["f1"] = (2 * languages[key]["precision"] * languages[key]["recall"]) / (
+                languages[key]["precision"] + languages[key]["recall"])
+        macro_f1 += languages[key]["f1"]
+        weight_f1 += languages[key]["f1"] * languages[key]["precisionDen"]
+
+    macro_f1 = macro_f1 / 6
+    weight_f1 = weight_f1 / number_tweet
+
+    eval_file_name = "eval_" + str(vocabulary) + "_" + str(whichGram) + "_" + str(smooth_value) + ".txt"
+
+    eval_file = open("evaluation_files/" + eval_file_name, 'w+', encoding='utf-8')
+    accuracy_line = str(accuracy_percentage) + "\n"
+    eval_file.write(accuracy_line)
+
+    precision_line = str(languages["eu"]["precision"]) + "  " + str(languages["ca"]["precision"]) + "  " + str(
+        languages["gl"]["precision"]) + "  " + str(
+        languages["es"]["precision"]) + "  " + str(languages["en"]["precision"]) + "  " + str(
+        languages["pt"]["precision"]) + "  " + "\n"
+    eval_file.write(precision_line)
+
+    recall_line = str(languages["eu"]["recall"]) + "  " + str(languages["ca"]["recall"]) + "  " + str(
+        languages["gl"]["recall"]) + "  " + str(
+        languages["es"]["recall"]) + "  " + str(languages["en"]["recall"]) + "  " + str(
+        languages["pt"]["recall"]) + "  " + "\n"
+    eval_file.write(recall_line)
+
+    f1_line = str(languages["eu"]["f1"]) + "  " + str(languages["ca"]["f1"]) + "  " + str(
+        languages["gl"]["f1"]) + "  " + str(
+        languages["es"]["f1"]) + "  " + str(languages["en"]["f1"]) + "  " + str(languages["pt"]["f1"]) + "  " + "\n"
+    eval_file.write(f1_line)
+
+    macro_average_line = str(macro_f1) + "  " + str(weight_f1)
+    eval_file.write(macro_average_line)
